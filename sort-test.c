ff24191e1ec04a70b97cee493fb7b971a95109fb
ps978 <ps978@cornell.edu>
2019-09-20 16:21:27 -0400

    Added new changes

--- sort-test.c
+++ sort-test.c
@@ -3,17 +3,18 @@
 
 void sort( int* x_ptr, int* y_ptr )
 {
-  if ( (*y_ptr) > (*x_ptr) ) {
+  if ( (*y_ptr) < (*x_ptr) ) {
     int temp = *x_ptr;
     *x_ptr   = *y_ptr;
     *y_ptr   = temp;
   }
+ 
 }
 
 int main( void )
 {
-  int a = 5;
-  int b = 9;
+  int a = 9;
+  int b = 5;
   sort( &a, &b );
   UTST_ASSERT_INT_EQ( a, 5 );
   UTST_ASSERT_INT_EQ( b, 9 );
